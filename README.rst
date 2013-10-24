49cf6a4bc90bb3dc3bbc508556e19c03bbcba576
Johan Bloemberg <johan.bloemberg@spilgames.com>
2013-10-24 15:36:11 +0200

    update

--- README.rst
+++ README.rst
@@ -1,43 +1,22 @@
-Flake8 TODO plugin
-==================
+Flake8 fileoutput plugin
+========================
 
-Check for TODO notes.
-
-This module provides a plugin for ``flake8``, the Python code checker.
-
-
-Installation
-------------
-
-You can install or upgrade ``flake8-todo`` with these commands::
-
-  $ pip install flake8-todo
-  $ pip install --upgrade flake8-todo
+Allows you to define a file to output the flake8 results to
 
 
 Plugin for Flake8
 -----------------
 
-When both ``flake8 2.0`` and ``flake8-todo`` are installed, the plugin is
+When both ``flake8 2.0`` and ``flake8-fileoutput`` are installed, the plugin is
 available in ``flake8``::
 
     $ flake8 --version
-    2.0 (pep8: 1.4.5, mccabe: 0.2, flake8-todo: 0.1, pyflakes: 0.6.1)
+    2.0 (pep8: 1.4.5, mccabe: 0.2, flake8-fileoutput: 0.1, pyflakes: 0.6.1)
 
 
 Changes
 -------
 
-0.3 - 2013-04-03
-````````````````
-* Fixed setup for Python 3.
-
-
-0.2 - 2013-03-26
-````````````````
-* Use unique entry point (previous version conflicted with mccabe)
-
-
-0.1 - 2013-03-26
+0.1 - 2013-10-24
 ````````````````
 * First release
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from __future__ import with_statement
 from setuptools import setup
 
 
-def get_version(fname='flake8_todo.py'):
+def get_version(fname='flake8_fileoutput.py'):
     with open(fname) as f:
         for line in f:
             if line.startswith('__version__'):
@@ -13,34 +13,33 @@ def get_version(fname='flake8_todo.py'):
 
 def get_long_description():
     descr = []
-    for fname in ('README.rst',):
+    for fname in ('README.rst', ):
         with open(fname) as f:
             descr.append(f.read())
     return '\n\n'.join(descr)
 
 
 setup(
-    name='flake8-todo',
+    name='flake8-fileoutput',
     version=get_version(),
-    description="TODO notes checker, plugin for flake8",  # noqa
+    description="File output plugin for flake8",  # noqa
     long_description=get_long_description(),
-    keywords='flake8 todo',
-    author='Marc Schlaich',
-    author_email='marc.schlaich@gmail.com',
-    url='https://github.com/schlamar/flake8-todo',
-    license='MIT',
-    py_modules=['flake8_todo'],
+    keywords='flake8 fileoutput',
+    author='Johan Bloemberg',
+    author_email='johan.bloemberg@spilgames.com',
+    url='https://github.com/spil-johan/flake8-fileoutput',
+    license='',
+    py_modules=['flake8_fileoutput'],
     zip_safe=False,
     entry_points={
         'flake8.extension': [
-            'flake8_todo = flake8_todo:check_todo_notes',
+            'flake8_fileoutput = flake8_fileoutput:FileOutput',
         ],
     },
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
